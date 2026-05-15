435f452c13b9236f799c2dc4db957b54b5b0a7de
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-05-15 11:27:31 +0900

    chore: simplify main method

--- src/main/java/dev/shoheiyamagiwa/shukan/Main.java
+++ src/main/java/dev/shoheiyamagiwa/shukan/Main.java
@@ -3,13 +3,13 @@ package dev.shoheiyamagiwa.shukan;
 import org.flywaydb.core.Flyway;
 
 public final class Main {
-	static void main() {
+	void main() {
 		migrateDatabase();
 		
 		// TODO: Implement Javalin application logic
 	}
 	
-	private static void migrateDatabase() {
+	private void migrateDatabase() {
 		String url = System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:postgresql://localhost:5432/shukan_dev");
 		String user = System.getenv().getOrDefault("JDBC_DATABASE_USERNAME", "shukan_user");
 		String password = System.getenv().getOrDefault("JDBC_DATABASE_PASSWORD", "dev_password");
